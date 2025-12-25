3c41d3304564d0d4f9d50a6cd5bdda909b77a070
Ibibo Manuel <manuel.ibibo@yahoo.com>
2025-12-25 16:21:57 +0100

    updated render external link to .env

--- db-init.sql
+++ db-init.sql
@@ -1,8 +1,15 @@
-CREATE TABLE IF NOT EXISTS farm_items (
+CREATE TABLE farm_products (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    description TEXT,
+    category VARCHAR(50),
+    product_name VARCHAR(100),
     quantity INTEGER,
-    price DECIMAL(10, 2),
-    image_url CHAR(255),
+    price DECIMAL(10,2),
+    image_url TEXT
 );
+
+-- Then insert without specifying id values
+INSERT INTO farm_products (category, product_name, quantity, price, image_url) VALUES
+('Pigs', 'Farm Pig', 50, 50000.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrcJ6smmt3n2zEjQOh7PQykzUwp2fBtq_GcA&s'),
+('Fish', 'Cat Fish', 75, 2500.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYx9y6KuKgfLNVzX2QuEOp8JaBBo-qJg_qHw&s'),
+('Chicken', 'Broiler', 50, 4500.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMUYofZw2HzOL4lvrjhI1xOkiEpY4iJjW1jQ&s'),
+('Eggs', 'Free-range eggs (crate)', 100, 5000.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-7fNeW-gJDzFfsIfE_SXEU2gIdq2Q6eHEVg&s');
