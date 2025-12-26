bd0a648fa7cc3befc33af78c9bafef8b0ff1ad74
Ibibo Manuel <manuel.ibibo@yahoo.com>
2025-12-26 17:11:09 +0100

    updated .env variable with external render link

--- db-init.sql
+++ db-init.sql
@@ -1,14 +1,14 @@
-CREATE TABLE farm_products (
+CREATE TABLE farm_items (
     id SERIAL PRIMARY KEY,
-    category VARCHAR(50),
-    product_name VARCHAR(100),
+    name VARCHAR(100),
+    description TEXT,
     quantity INTEGER,
     price DECIMAL(10,2),
     image_url TEXT
 );
 
 -- Then insert without specifying id values
-INSERT INTO farm_products (category, product_name, quantity, price, image_url) VALUES
+INSERT INTO farm_items (name, description, quantity, price, image_url) VALUES
 ('Pigs', 'Farm Pig', 50, 50000.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrcJ6smmt3n2zEjQOh7PQykzUwp2fBtq_GcA&s'),
 ('Fish', 'Cat Fish', 75, 2500.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYx9y6KuKgfLNVzX2QuEOp8JaBBo-qJg_qHw&s'),
 ('Chicken', 'Broiler', 50, 4500.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMUYofZw2HzOL4lvrjhI1xOkiEpY4iJjW1jQ&s'),
